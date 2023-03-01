08333a5fd527212c0cffbfd9bd01f2de844422b3
masutay <masutay76@gmail.com>
2023-03-01 06:51:09 +0300

    add selected class

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let btnCountPlus = document.querySelectorAll(".count-plus");
 let btnCountMinus = document.querySelectorAll(".count-minus");
 let inputTicket = document.querySelectorAll(".count-result");
+
 let goSelectSeat = document.querySelector(".btn-continue");
 let bookingBody = document.querySelector(".booking-body");
 let info = document.querySelector(".info");
 
+let seats = document.querySelectorAll(".seat:not(.reserved)");
+
 let ticketPrice;
+//let totalTicketNum;
 
 const countPlus = [];
 const inputResult = [];
+
+const seatArr = [];
+const selectedSeatArr = [1, 2];
 
 inputTicket.forEach(function (input) {
   inputResult.push(input);
@@ -24,6 +31,7 @@ for (let index in countPlus) {
       console.log("10 biletten fazla rezervasyon yapılamaz!!");
     } else {
       inputResult[index].value++;
+      
 
       if (inputResult[index].value > 0) {
         goSelectSeat.classList.remove("disabled");
@@ -41,3 +49,20 @@ goSelectSeat.addEventListener("click", function () {
   info.style.visibility = "visible";
 });
 
+seats.forEach(function (seat) {
+  seatArr.push(seat);
+});
+
+for (let key in seatArr) {
+  seatArr[key].addEventListener("click", function () {
+    // for (let i = 0; i < key; i++) {
+    //   if (totalTicketNum < selectedSeatArr.length) {
+    //     console.log(`max ${selectedSeatArr.length} koltuk seçebilirsiniz`);
+    //   } else {
+        seatArr[key].classList.add("seat-selected");
+
+        console.log(selectedSeatArr.length);
+      
+    
+  });
+}
